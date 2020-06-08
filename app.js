b2f3a35d1e344442f15d6da679472014b9724398
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-06-07 21:46:45 -0700

    wrote classes for each employee lib file, wrote question prompts array, async init function to start inquirer, if else statements for employee roles, and fs.writeFile function in app.js file

--- app.js
+++ app.js
@@ -14,22 +14,58 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const questionPrompts = [
+    { 
+      type: "input",
+      name: "name",
+      message: "What is your teammate's name?",
+    },
+    { 
+      type: "input",
+      name: "id",
+      message: "What is your teammate's ID?"
+    },
+    { 
+      type: "input",
+      name: "email",
+      message: "What is your teammate's email?"
+    },
+    { 
+      type: "list",
+      name: "role",
+      message: "What is your teammate's role?",
+      choices: ["Manager", "Engineer", "Intern"],
+    }
+  ];
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+   // Answers array
+   const employeeArray = [];
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+   // Note: write code to ask different questions via inquirer depending on employee type.
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+   // Create async init function
+    async function init() {
+    const employeeInput = await inquirer.prompt(questionPrompts);
+    const {name, id, email, role} = employeeInput;     
+    // If else statement based on employee roles: manager, engineer, intern  
+    // if (role === "Manager") { }
+    // else if (role === "Engineer") { } 
+    // else if (role === "Intern") { } 
+
+    // push employee constructor object into employeesArray
+    // employeeArray.push();
+
+    // After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; 
+    // `render` function will generate and return a block of HTML including templated divs for each employee and store generated HTML in a const
+    const allEmployees = render(employeeArray);
+
+    // Now ready to create an HTML file using the HTML returned from the `render` function. 
+    // Write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location.
+    fs.writeFile(outputPath, allEmployees, function(err){
+        if(err){
+          throw err;
+        }
+      });
+  };
+
+init();
