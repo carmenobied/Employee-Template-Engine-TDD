82b8b54f23a2ee732acf811f75730de40b38b49f
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-06-10 22:12:32 -0700

    app functionioning correctly with question prompts and pushing the user input to the html. Next step: currently overwrites previous employee when entering new employee. Need to have it ask user if they want to enter additionally employee(s) and have the questions start again and render to employee list html

--- app.js
+++ app.js
@@ -14,92 +14,110 @@ const render = require("./lib/htmlRenderer");
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 const questionPrompts = [
-    { 
-      type: "input",
-      name: "name",
-      message: "What is your employee's name?",
-      validate: function(response) {
-        if (response.length < 1) {
-            return 'Please enter valid name';
-        }
-        return true;
+  {
+    type: "input",
+    name: "name",
+    message: "What is your employee's name?",
+    validate: function (response) {
+      if (response.length < 1) {
+        return 'Please enter valid name';
       }
-    },
-    { 
-      type: "input",
-      name: "id",
-      message: "What is your teammate's ID number?",
-      validate: function(response) {
-        if (response = NaN || response.length < 1) {
-            return 'Please enter valid ID number';
-        }
-        return true;
-      }
-    },
-    { 
-      type: "input",
-      name: "email",
-      message: "What is your employee's email address?",
-      validate: function(response) {
-        if (response.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-            return true;
-        } else {
-            return 'Invalid email. Please enter valid email';
-        };
-      }
-    },
-    { 
-      type: "list",
-      name: "role",
-      message: "What is your employee's role?",
-      choices: ["Manager", "Engineer", "Intern"],
+      return true;
     }
-  ];
-  // Role-based prompts
-  const managerPrompt= {
+  },
+  {
     type: "input",
-    name: "officeNumber",
-    message: "What is your manager's office number?", 
-  };
-  const engineerPrompt= {
+    name: "id",
+    message: "What is your teammate's ID number?",
+    validate: function (response) {
+      if (response = NaN || response.length < 1) {
+        return 'Please enter valid ID number';
+      }
+      return true;
+    }
+  },
+  {
     type: "input",
-    name: "gitUsername",
-    message: "What is your engineer's Github username?", 
+    name: "email",
+    message: "What is your employee's email address?",
+    validate: function (response) {
+      if (response.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+        return true;
+      } else {
+        return 'Invalid email. Please enter valid email';
+      };
+    }
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is your employee's role?",
+    choices: ["Manager", "Engineer", "Intern"],
+  }
+];
+// Role-based prompts
+const managerPrompt = {
+  type: "input",
+  name: "officeNumber",
+  message: "What is your manager's office number?",
+};
+const engineerPrompt = {
+  type: "input",
+  name: "githubUsername",
+  message: "What is your engineer's GitHub username?",
+};
+const internPrompt = {
+  type: "input",
+  name: "internSchool",
+  message: "What school does the intern go to?",
+};
+
+// Answers array 
+const employeeList = [];
+
+// Use different question prompts via inquirer based on employee type.
+// Create async init function
+async function init() {
+  const employeeInput = await inquirer.prompt(questionPrompts);
+  const { name, id, email, role } = employeeInput;
+
+  // Use if else statements based on employee roles: manager, engineer, intern  
+
+  // MANAGER
+  if (role === "Manager") {
+    const officeNumPrompt = await inquirer.prompt(managerPrompt);
+    const officeNum = officeNumPrompt.number;
+
+    const employeeInfo = new Manager(name, id, email, officeNum);
+    employeeList.push(employeeInfo); // push employeeInfo constructor object into employeeList array
+
+    // ENGINEER
+  } else if (role === "Engineer") {
+    const githubPrompt = await inquirer.prompt(engineerPrompt);
+    const githubUser = githubPrompt.githubUsername;
+
+    const employeeInfo = new Engineer(name, id, email, githubUser);
+    employeeList.push(employeeInfo);
+
+    // INTERN 
+  } else if (role === "Intern") {
+    const schoolPrompt = await inquirer.prompt(internPrompt);
+    const schoolInfo = schoolPrompt.schoolIntern
+    const employeeInfo = new Intern(name, id, email, schoolInfo);
+    employeeList.push(employeeInfo);
   };
-  const internPrompt= {
-    type: "input",
-    name: "internSchool",
-    message: "What school does the intern go to?",
-  };
 
-   // Answers array 
-   const employeeList = [];
+  // After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; 
+  // `render` function will generate and return a block of HTML including templated divs for each employee and store generated HTML in a const
+  const allEmployees = render(employeeList);
 
-   // Note: write code to ask different questions via inquirer depending on employee type.
-
-   // Create async init function
-    async function init() {
-    const employeeInput = await inquirer.prompt(questionPrompts);
-    const {name, id, email, role} = employeeInput;     
-    // If else statement based on employee roles: manager, engineer, intern  
-    // if (role === "Manager") { }
-    // else if (role === "Engineer") { } 
-    // else if (role === "Intern") { } 
-
-    // push employee constructor object into employeesList
-    // employeeList.push();
-
-    // After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; 
-    // `render` function will generate and return a block of HTML including templated divs for each employee and store generated HTML in a const
-    const allEmployees = render(employeeList);
-
-    // Now ready to create an HTML file using the HTML returned from the `render` function. 
-    // Write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location.
-    fs.writeFile(outputPath, allEmployees, function(err){
-        if(err){
-          throw err;
-        }
-      });
-  };
+  // Now ready to create an HTML file using the HTML returned from the `render` function. 
+  // Write it to a file named `team.html` in the `output` folder. You can use the variable `outputPath` above target this location.
+  fs.writeFile(outputPath, allEmployees, function (err) {
+    if (err) {
+      throw err;
+    }
+  });
+};
 
 init();
